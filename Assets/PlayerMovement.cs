717ee01414587d000e083938b51441f6b132f89e
GavinAshworth <gavinashworth315@gmail.com>
2025-03-01 12:28:05 -0800

    Player Movement Bug Fix
    I forgot to incorporate the rotation of the player into how movement is calculated, did that now

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,7 +4,7 @@ using Unity.Cinemachine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 10f;
     [SerializeField] private CinemachineCamera freeLookCamera;
     private Rigidbody rb;
     private Vector2 moveInput;
@@ -21,14 +21,29 @@ public class PlayerMovement : MonoBehaviour
         updateRotation();
     }
 
-    private void updateMovement(){
-        //Move input is vector 2 so we change to vector 3 and sub in our current y velocity for y
-        rb.linearVelocity = new Vector3(moveInput.x * speed, rb.linearVelocity.y, moveInput.y * speed);
+     private void updateMovement()
+    {
+        // This gets the cameras forward and side to side components so our players movements match the camera (We ignore the y so the player can look up and down without affecting falling or jumping)
+        Vector3 cameraForward = freeLookCamera.transform.forward;
+        Vector3 cameraRight = freeLookCamera.transform.right;
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        // This calcs move direction relative t othe camera
+        Vector3 moveDirection = (cameraForward * moveInput.y + cameraRight * moveInput.x).normalized;
+
+        rb.linearVelocity = new Vector3(moveDirection.x * speed, rb.linearVelocity.y, moveDirection.z * speed);
     }
 
-    private void updateRotation(){
-        transform.forward = freeLookCamera.transform.forward;
-        transform.rotation = Quaternion.Euler(0,transform.rotation.eulerAngles.y,0);
+    private void updateRotation()
+    {
+        // Similarr to bowling, but we are using transform 
+        Vector3 cameraForward = freeLookCamera.transform.forward;
+        cameraForward.y = 0; // Ignore vertical rotation
+        cameraForward.Normalize();
+        transform.forward = cameraForward;
     }
     public void Move(InputAction.CallbackContext context){
         moveInput = context.ReadValue<Vector2>();
